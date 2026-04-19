02939ed9af870580416efbc26479ceb18c5a6f00
Anant Saini <anantsaini.india@gmail.com>
2026-04-20 01:46:20 +0530

    refactor: update user component with signal rendering instead of state change detection rendering

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../../../public/resources/dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -6,4 +9,12 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+export class User {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => `images/users/${this.selectedUser().avatar}`);
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
