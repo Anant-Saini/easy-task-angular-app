15b26a145244a83e280a930b7da097eb37a9fdbc
Anant Saini <anantsaini.india@gmail.com>
2026-04-22 20:57:00 +0530

    feat: add user componnet highlighting when a user's task are selected or being viewed

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -12,6 +12,7 @@ import { type User } from './user-model';
 export class UserComponent {
 
   @Input({ required: true}) user!: User;
+  @Input({ required: true}) selected!: boolean;
   @Output() selectUser = new EventEmitter<User>();
 
 
