a8348fb81f4c5d53d89e43e24bff3a2182047f7f
Anant Saini <anantsaini.india@gmail.com>
2026-04-23 17:21:13 +0530

    refactor: add more rexognizable date format in task via pipe

--- src/app/tasks/task-component/task-component.ts
+++ src/app/tasks/task-component/task-component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task-model';
 import { Card } from "../../shared/card/card";
+import { DatePipe } from '@angular/common'; 
 
 @Component({
   selector: 'app-task-component',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task-component.html',
   styleUrl: './task-component.css',
 })
